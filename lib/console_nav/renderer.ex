8a266e10c1a62db3638b4732b76998f956d158a7
robot <cylehunter33@gmail.com>
2017-02-28 22:28:46 -0600

    Simplify renderer. Updated textures.
    The draw_line and clear_screen methods were writing independently of the
    wallet and keyboard controls. I wanted to have one simple IO.write call
    that wrapped the various draw methods that were responsible for
    populating their own character lists. This simplifies the code quite a
    bit. I also updated the textures characters for the walls and players
    and increased map spacing so it's not so claustrophobic.

--- lib/console_nav/renderer.ex
+++ lib/console_nav/renderer.ex
@@ -5,68 +5,56 @@ defmodule ConsoleNav.Renderer do
   use GenServer
 
   @refresh_interval 100
-  @coin_texture "$"
-  @player_texture "@"
-  @wall_texture "#"
+  @coin "$ "
+  @player "\x{2588}\x{2588}"
+  @wall "\x{2588}\x{2588}"
 
-  def start_link do
-    GenServer.start_link(__MODULE__, nil)
-  end
+  def start_link, do: GenServer.start_link(__MODULE__, nil)
 
-  def init(_) do
-    draw
-  end
+  def init(_), do: loop
 
-  defp draw do
+  defp loop do
     state = GameData.state
-    clear_board
-    state.board
-    |> Matrix.to_list
-    |> Enum.with_index
-    |> Enum.each(&draw_line/1)
     IO.write [
-      IO.ANSI.reset,
-      draw_wallet(state),
+      clear_screen,
+      draw_board(state.board),
+      draw_wallet(state.wallet),
       draw_controls
     ]
     :timer.sleep @refresh_interval
-    draw
+    loop
+  end
+
+  defp draw_board(board) do
+    Matrix.to_list(board)
+    |> Enum.with_index
+    |> Enum.map(fn(line) -> ["\r", draw_line(line), "\n"] end)
   end
 
   defp draw_line(line) do
     {row, x} = line
-    IO.write "#{IO.ANSI.clear_line}\r"
-    row
-    |> Enum.with_index
+    Enum.with_index(row)
     |> Enum.map(fn({col, y}) -> draw_cell(col, {x, y}) end)
-    |> Enum.join(" ")
-    |> IO.puts
   end
 
-  defp draw_cell(char, coords) do
-    player_location = Navigator.state
-    if coords == player_location do
-      [IO.ANSI.bright, IO.ANSI.blue, @player_texture, IO.ANSI.reset]
+  defp draw_cell(char, pos) do
+    if pos == Navigator.state do
+      [IO.ANSI.blue, @player, IO.ANSI.reset]
     else
       case char do
-        3 -> [IO.ANSI.bright, IO.ANSI.yellow, @coin_texture, IO.ANSI.reset]
-        1 -> [IO.ANSI.white, @wall_texture]
-        _ -> [IO.ANSI.black, " "]
+        3 -> [IO.ANSI.bright, IO.ANSI.yellow, @coin, IO.ANSI.reset]
+        1 -> [IO.ANSI.white, @wall]
+        _ -> [IO.ANSI.black, "  "]
       end
     end
   end
 
-  defp clear_board do
-    IO.write [
-      IO.ANSI.clear,
-      IO.ANSI.home
-    ]
-  end
+  defp clear_screen, do: [IO.ANSI.clear, IO.ANSI.home]
 
-  defp draw_wallet(state) do
-    wallet = state.wallet
+  defp draw_wallet(wallet) do
     [
-     "\n#{IO.ANSI.clear_line}\r",
+      IO.ANSI.reset,
+      "\n#{IO.ANSI.clear_line}\r",
       "Wallet: ",
       IO.ANSI.bright,
       IO.ANSI.yellow,
