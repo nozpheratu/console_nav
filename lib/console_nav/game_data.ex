4415cdf9a306092ae12674109f186715a8a2f029
robot <cylehunter33@gmail.com>
2017-03-05 16:42:58 -0600

    Generate coins procedurally
    All coins on the map will now be generated procedurally to spice things
    up a bit.

--- lib/console_nav/game_data.ex
+++ lib/console_nav/game_data.ex
@@ -10,25 +10,26 @@ defmodule ConsoleNav.GameData do
   # 2: player
   # 3: coin
   @initial_board [
-    [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 1, 3, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 3],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
     [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-    [3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]
 
   def start(board \\ @initial_board) do
+    board = add_coins(board, 0)
     state = %{board: Matrix.from_list(board), wallet: 0}
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
@@ -39,5 +40,25 @@ defmodule ConsoleNav.GameData do
 
   def handle_cast({:set, assigned_state}, state) do
     {:noreply, Map.merge(state, assigned_state)}
+  end
+
+  defp add_coins(board, sanity) do
+    attempt = board
+    |> Enum.map(fn(row) -> Enum.map(row, &generate_coins/1) end)
+    count = count_coins(attempt)
+    if(sanity > 10000 || enough_coins?(count), do: attempt, else: add_coins(board, sanity + 1))
+  end
+
+  defp count_coins(board) do
+    Enum.flat_map(board, &(&1))
+    |> List.foldl(0, fn(x, acc) -> if(x == 3, do: acc + 1, else: acc) end)
+  end
+
+  defp enough_coins?(count) do
+    count >= 5 && count <= 12
+  end
+
+  defp generate_coins(col) do
+    if(col == 0 && Enum.random(1..100) > 95, do: Enum.random([col, 3]), else: col)
   end
 end
